eaa3b579d5968fbc1f2bd9d579bec9b5126b5bed
LEE YEJIN <dldulwls@gmail.com>
2024-03-04 14:14:16 +0900

    [FEAT]: 회원가입 기능 구현

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { FormButton } from "../styles/FormButton";
+import axios from "axios";
 // import CommonForm from "../components/CommonForm";
 
 function SignUp() {
   const [userId, setUserId] = useState("");
   const [userPw, setUserPw] = useState("");
+  const [error, setError] = useState("");
 
   const handleUserIdChange = (e) => {
     setUserId(e.target.value);
@@ -14,20 +16,50 @@ function SignUp() {
     setUserPw(e.target.value);
   };
 
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post("http://3.38.191.164/register", {
+        id: userId,
+        password: userPw,
+      });
+
+      //존재하는 아이디가 있는 지 확인
+      // if (userId === id) {
+      // }
+
+      console.log(response);
+      if (response.status === 201) {
+        console.log("회원가입 성공");
+        setUserId("");
+        setUserPw("");
+      } else if (response.status === 401) {
+        setError(response.data.message);
+      } else {
+        setError("알 수 없는 오류가 발생했습니다.");
+      }
+      // setUserId(response);
+    } catch (error) {
+      alert("서버 에러 발생!");
+      setUserId("");
+      setUserPw("");
+    }
+  };
+
   return (
     <>
       <Title>회원가입</Title>
-      <FormContainer>
-        <label for="id">아이디</label>
+      <FormContainer onClick={(e) => e.preventDefault()}>
+        <label htmlFor="id">아이디</label>
         <FormInput type="text" id="id" onChange={handleUserIdChange} />
-        <label for="password">비밀번호</label>
+        <label htmlFor="password">비밀번호</label>
         <FormInput
           type="password"
           id="password"
           onChange={handleUserPwChange}
         />
-        <FormButton>회원가입</FormButton>
+        <FormButton onClick={handleRegister}>회원가입</FormButton>
         <FormButton>로그인하기</FormButton>
+        {error && <div>{error}</div>}
       </FormContainer>
     </>
   );
